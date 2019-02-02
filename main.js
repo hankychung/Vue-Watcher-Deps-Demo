a0d86a97a5f70d2c4eab7e8a6109fe4fb794f675
Han <hanky_chung@hotmail.com>
2019-02-02 13:10:48 +0800

    finish

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ let man = {
   wealth: 1000000
 }
 
+class Dep {
+  constructor() {
+    this.deps = []
+  }
+  getDeps() {
+    if (!Dep.target || this.deps.includes(Dep.target)) return
+    console.log('依赖添加', Dep.target)
+    this.deps.push(Dep.target)
+  }
+  update() {
+    this.deps.forEach(dep => {
+      dep()
+    })
+  }
+}
+
 class Observer {
   constructor(obj) {
     this.walk(obj)
@@ -29,19 +45,56 @@ class Observer {
     })
   }
   defineReactive(obj, prop, val) {
+    let dep = new Dep()
     Object.defineProperty(obj, prop, {
       get() {
         console.log(`${prop} - 被读取！`)
+        dep.getDeps()
         return val
       },
       set(newVal) {
         if (newVal == val) return
         val = newVal
         console.log(`${prop} - 被修改！`)
+        dep.update()        
       }
     })
   }
 }
 
-let ins = new Observer(man)
-// console.dir(ins)
+new Observer(man)
+
+class Watcher {
+  constructor(obj, prop, computed, callback) {
+    this.getVal(obj, prop, computed, callback)
+  }
+
+  getVal(obj, prop, computed, callback) {
+    Object.defineProperty(obj, prop, {
+      get() {
+        Dep.target = callback
+        console.log(`computed属性 - ${prop}被读取！`)
+        return computed()
+      },
+      set() {
+        console.error('计算属性不可被修改！')
+      }
+    })
+  }
+}
+
+new Watcher(man, 'strength', () => {
+  let {height, weight} = man
+  if (height > 160 && weight > 70) return 'strong'
+  return 'weak'
+}, () => {
+  alert(`i am so ${man.strength} !`)
+})
+
+new Watcher(man, 'isGreat', () => {
+  let {height, weight, wealth} = man
+  if (height > 180 && weight > 70 && wealth > 10000000) return 'Great!'
+  return 'not good enough ...'
+}, () => {
+  alert(`they say i am ${man.isGreat}`)
+})
